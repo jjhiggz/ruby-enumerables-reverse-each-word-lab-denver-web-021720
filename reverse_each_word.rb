4021bfe2b97ae43eb47d8952acbb2e5b117b5119
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-02 08:17:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
 def reverse_each_word(string)
-a = string.split
-a.map do |n|
-  a[n].reverse
-end
-b = a.join(' ')
+  a = string.split
+  d = a.map do |n|
+  n.reverse
+  end
+  b = d.join(' ')
+  puts b
+
 return b
 end
+
+
